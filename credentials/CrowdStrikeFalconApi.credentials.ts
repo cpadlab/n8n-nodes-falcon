a1cda916ab5f0569b5cd99f126e25536b4fed144
cpadlab <cpadlab@gmail.com>
2026-07-29 10:01:26 +0200

    feat: Add CrowdStrike Falcon API credential

---
+++ credentials/CrowdStrikeFalconApi.credentials.ts
@@ -0,0 +1,42 @@
+import type { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class CrowdStrikeFalconApi implements ICredentialType {
+	name = 'crowdStrikeFalconApi';
+	displayName = 'CrowdStrike Falcon API';
+	documentationUrl = 'https://developer.crowdstrike.com/sdks/typescript/';
+	properties: INodeProperties[] = [
+		{
+			displayName: 'Cloud Region',
+			name: 'cloud',
+			type: 'options',
+			options: [
+				{ name: 'US-1', value: 'us-1' },
+				{ name: 'US-2', value: 'us-2' },
+				{ name: 'EU-1', value: 'eu-1' },
+				{ name: 'US-GOV-1', value: 'us-gov-1' },
+			],
+			default: 'eu-1',
+			required: true,
+			description: 'The CrowdStrike cloud environment/region to connect to',
+		},
+		{
+			displayName: 'Client ID',
+			name: 'clientId',
+			type: 'string',
+			default: '',
+			required: true,
+			description: 'API Client ID generated in Falcon Console under API Scopes',
+		},
+		{
+			displayName: 'Client Secret',
+			name: 'clientSecret',
+			type: 'string',
+			typeOptions: {
+				password: true,
+			},
+			default: '',
+			required: true,
+			description: 'API Client Secret associated with the Client ID',
+		},
+	];
+}
