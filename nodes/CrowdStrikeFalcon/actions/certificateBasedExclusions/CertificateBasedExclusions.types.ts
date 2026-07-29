79bb276a11deee79b321e8cf9429f6c74e724ad2
cpadlab <cpadlab@gmail.com>
2026-07-29 13:32:18 +0200

    feat: Add Certificate Based Exclusions TypeScript interfaces

---
+++ nodes/CrowdStrikeFalcon/actions/certificateBasedExclusions/CertificateBasedExclusions.types.ts
@@ -0,0 +1,10 @@
+export interface ICbExclusionsGenericParams {
+	ids?: string[];
+	id?: string;
+	comment?: string;
+	filter?: string;
+	offset?: number;
+	limit?: number;
+	sort?: string;
+	body?: Record<string, any>;
+}
