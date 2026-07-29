ffae1e2ff6d7a07987d805b78df9bb5a58831e32
cpadlab <cpadlab@gmail.com>
2026-07-29 13:33:02 +0200

    feat: Add Cloud Connect Aws TypeScript interfaces

---
+++ nodes/CrowdStrikeFalcon/actions/cloudConnectAws/CloudConnectAws.types.ts
@@ -0,0 +1,9 @@
+export interface ICloudConnectAwsGenericParams {
+	ids?: string[];
+	limit?: number;
+	offset?: number;
+	sort?: string;
+	filter?: string;
+	mode?: string;
+	body?: Record<string, any>;
+}
