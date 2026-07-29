bd7c96a8a66149e159d4bf9c522a9928e8363f19
cpadlab <cpadlab@gmail.com>
2026-07-29 13:33:16 +0200

    feat: Add Cloud Google Cloud Registration TypeScript interfaces

---
+++ nodes/CrowdStrikeFalcon/actions/cloudGoogleCloudRegistration/CloudGoogleCloudRegistration.types.ts
@@ -0,0 +1,9 @@
+export interface ICloudGoogleCloudRegistrationGenericParams {
+	ids?: string;
+	idsArray?: string[];
+	filter?: string;
+	sort?: string;
+	limit?: number;
+	offset?: number;
+	body?: Record<string, any>;
+}
