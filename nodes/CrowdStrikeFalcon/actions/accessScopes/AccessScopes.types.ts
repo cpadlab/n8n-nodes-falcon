6093b4e9ead20a20d05193d4b9984cab7338385a
cpadlab <cpadlab@gmail.com>
2026-07-29 12:20:10 +0200

    feat[accessScopes]: Add AccessScopes TypeScript interfaces

---
+++ nodes/CrowdStrikeFalcon/actions/accessScopes/AccessScopes.types.ts
@@ -0,0 +1,10 @@
+export interface IQueryAccessScopesParams {
+	filter?: string;
+	sort?: string;
+	offset?: number;
+	limit?: number;
+}
+
+export interface IListAccessScopesParams {
+	ids?: string[];
+}
