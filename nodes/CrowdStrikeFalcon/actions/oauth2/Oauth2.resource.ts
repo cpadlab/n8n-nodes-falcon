c8898565849882cab3172a512123d157abc60d1b
cpadlab <cpadlab@gmail.com>
2026-07-30 09:11:28 +0200

    feat: Add Oauth2 operations and fields

---
+++ nodes/CrowdStrikeFalcon/actions/oauth2/Oauth2.resource.ts
@@ -0,0 +1,84 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const oauth2Operations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['oauth2'],
+			},
+		},
+		options: [
+			{ name: 'OAuth2 Access Token', value: 'oauth2AccessToken', description: 'Generate an OAuth2 access token', action: 'OAuth2 access token' },
+			{ name: 'OAuth2 Revoke Token', value: 'oauth2RevokeToken', description: 'Revoke a previously issued OAuth2 access token', action: 'OAuth2 revoke token' },
+		],
+		default: 'oauth2AccessToken',
+	},
+];
+
+export const oauth2Fields: INodeProperties[] = [
+	{
+		displayName: 'Client ID',
+		name: 'clientId',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['oauth2'],
+				operation: ['oauth2AccessToken', 'oauth2RevokeToken'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'OAuth2 Client ID',
+	},
+	{
+		displayName: 'Client Secret',
+		name: 'clientSecret',
+		type: 'string',
+		typeOptions: {
+			password: true,
+		},
+		displayOptions: {
+			show: {
+				resource: ['oauth2'],
+				operation: ['oauth2AccessToken'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'OAuth2 Client Secret',
+	},
+	{
+		displayName: 'Member CID',
+		name: 'memberCid',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['oauth2'],
+				operation: ['oauth2AccessToken'],
+			},
+		},
+		default: '',
+		description: 'Child Customer ID for MSSP environments',
+	},
+	{
+		displayName: 'Token',
+		name: 'token',
+		type: 'string',
+		typeOptions: {
+			password: true,
+		},
+		displayOptions: {
+			show: {
+				resource: ['oauth2'],
+				operation: ['oauth2RevokeToken'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'Access token to revoke',
+	},
+];
