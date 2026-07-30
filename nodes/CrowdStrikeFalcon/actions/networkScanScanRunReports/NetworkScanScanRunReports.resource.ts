959a873dbc2ed331992186feb8407f3410a2d74a
cpadlab <cpadlab@gmail.com>
2026-07-30 09:09:45 +0200

    feat: Add Network Scan Scan Run Reports operations and fields

---
+++ nodes/CrowdStrikeFalcon/actions/networkScanScanRunReports/NetworkScanScanRunReports.resource.ts
@@ -0,0 +1,49 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const networkScanScanRunReportsOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['networkScanScanRunReports'],
+			},
+		},
+		options: [
+			{ name: 'Get Scan Run Reports', value: 'getScanRunReports', description: 'Downloads scan run report in CSV format', action: 'Get scan run reports' },
+		],
+		default: 'getScanRunReports',
+	},
+];
+
+export const networkScanScanRunReportsFields: INodeProperties[] = [
+	{
+		displayName: 'Scan Run ID',
+		name: 'id',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['networkScanScanRunReports'],
+				operation: ['getScanRunReports'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'Unique scan run ID',
+	},
+	{
+		displayName: 'X-CS-USERUUID',
+		name: 'xCSUSERUUID',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['networkScanScanRunReports'],
+				operation: ['getScanRunReports'],
+			},
+		},
+		default: '',
+		description: 'User UUID header override',
+	},
+];
