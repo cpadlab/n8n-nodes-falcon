5f13ce3581b1f88811d2923772e381e606392e57
cpadlab <cpadlab@gmail.com>
2026-07-30 10:10:20 +0200

    feat: Add Sensor Visibility Exclusions TypeScript interfaces

---
+++ nodes/CrowdStrikeFalcon/actions/sensorVisibilityExclusions/SensorVisibilityExclusions.types.ts
@@ -0,0 +1,9 @@
+export interface ISensorVisibilityExclusionsGenericParams {
+	ids?: string[];
+	comment?: string;
+	filter?: string;
+	sort?: string;
+	offset?: number;
+	limit?: number;
+	body?: Record<string, any> | Array<any>;
+}
