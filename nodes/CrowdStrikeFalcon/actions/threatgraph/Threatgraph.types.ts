fe49e8b8ba916b83b6daf920af02317931504ebd
cpadlab <cpadlab@gmail.com>
2026-07-30 10:12:33 +0200

    feat: Add Threatgraph TypeScript interfaces

---
+++ nodes/CrowdStrikeFalcon/actions/threatgraph/Threatgraph.types.ts
@@ -0,0 +1,13 @@
+export interface IThreatgraphGenericParams {
+	id?: string;
+	ids?: string | string[];
+	value?: string;
+	type?: string;
+	edgeType?: string;
+	vertexType?: string;
+	direction?: string;
+	scope?: string;
+	nano?: boolean;
+	offset?: string;
+	limit?: number;
+}
