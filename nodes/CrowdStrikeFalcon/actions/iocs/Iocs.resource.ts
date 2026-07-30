0a49b7e0f52a79a41ef9dd03cf8a9c53d89ec130
cpadlab <cpadlab@gmail.com>
2026-07-30 09:07:16 +0200

    feat: Add Iocs operations and fields

---
+++ nodes/CrowdStrikeFalcon/actions/iocs/Iocs.resource.ts
@@ -0,0 +1,107 @@
+import type { INodeProperties } from 'n8n-workflow';
+
+export const iocsOperations: INodeProperties[] = [
+	{
+		displayName: 'Operation',
+		name: 'operation',
+		type: 'options',
+		noDataExpression: true,
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+			},
+		},
+		options: [
+			{ name: 'Devices Count', value: 'devicesCount', description: 'Number of hosts in your customer account that have observed a given custom IOC', action: 'Devices count' },
+			{ name: 'Devices Ran On', value: 'devicesRanOn', description: 'Find hosts that have observed a given custom IOC', action: 'Devices ran on' },
+			{ name: 'Entities Processes', value: 'entitiesProcesses', description: 'For the provided ProcessID retrieve the process details', action: 'Entities processes' },
+			{ name: 'Processes Ran On', value: 'processesRanOn', description: 'Search for processes associated with a custom IOC', action: 'Processes ran on' },
+		],
+		default: 'devicesCount',
+	},
+];
+
+export const iocsFields: INodeProperties[] = [
+	{
+		displayName: 'IOC Type',
+		name: 'type',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+				operation: ['devicesCount', 'devicesRanOn', 'processesRanOn'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'IOC type (e.g. sha256, md5, domain, ipv4)',
+	},
+	{
+		displayName: 'IOC Value',
+		name: 'value',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+				operation: ['devicesCount', 'devicesRanOn', 'processesRanOn'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'IOC value string',
+	},
+	{
+		displayName: 'Device ID',
+		name: 'deviceId',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+				operation: ['processesRanOn'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'Target Device ID',
+	},
+	{
+		displayName: 'Process IDs',
+		name: 'ids',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+				operation: ['entitiesProcesses'],
+			},
+		},
+		default: '',
+		required: true,
+		description: 'Comma-separated list of Process IDs',
+	},
+	{
+		displayName: 'Limit',
+		name: 'limit',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+				operation: ['devicesRanOn', 'processesRanOn'],
+			},
+		},
+		default: '100',
+		description: 'Max number of results to return',
+	},
+	{
+		displayName: 'Offset',
+		name: 'offset',
+		type: 'string',
+		displayOptions: {
+			show: {
+				resource: ['iocs'],
+				operation: ['devicesRanOn', 'processesRanOn'],
+			},
+		},
+		default: '',
+		description: 'Starting index or pagination offset',
+	},
+];
