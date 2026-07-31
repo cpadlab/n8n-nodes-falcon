12340f90c47227d26b135cda224a32cad0b177b7
cpadlab <cpadlab@gmail.com>
2026-07-31 08:46:19 +0200

    refactor: Simplify Falcon Container resource definitions

--- nodes/CrowdStrikeFalcon/actions/falconContainer/FalconContainer.resource.ts
+++ nodes/CrowdStrikeFalcon/actions/falconContainer/FalconContainer.resource.ts
@@ -1,21 +1,13 @@
 import type { INodeProperties } from 'n8n-workflow';
 
+import { createOperationField } from '../common';
+
 export const falconContainerOperations: INodeProperties[] = [
-	{
-		displayName: 'Operation',
-		name: 'operation',
-		type: 'options',
-		noDataExpression: true,
-		displayOptions: {
-			show: {
-				resource: ['falconContainer'],
-			},
-		},
-		options: [
+	createOperationField('falconContainer', [
+
 			{ name: 'Get Credentials', value: 'getCredentials', description: 'Gets the registry credentials', action: 'Get credentials' },
-		],
-		default: 'getCredentials',
-	},
+		
+	], 'getCredentials'),
 ];
 
 export const falconContainerFields: INodeProperties[] = [];
